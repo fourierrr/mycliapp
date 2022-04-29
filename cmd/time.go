5d1dffcdce75afc62ceb4c839fc82b65226b1cfb
chenkai <fourierrr@gmail.com>
2022-04-29 20:45:20 +0800

    first commit

---
+++ cmd/time.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// timeCmd represents the time command
+var timeCmd = &cobra.Command{
+	Use:   "time",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if flag, _ := cmd.Flags().GetBool("str"); flag {
+			fmt.Println(time.Now.Format("2006-01-02"))
+			return
+		}
+		fmt.Println(time.Now())
+		// fmt.Println("time called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(timeCmd)
+	timeCmd.Flags().BoolP("str", "s", false, "show time in format in 2006-01-02")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// timeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// timeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
